6c7aee7775870824a04b19efc21f06a8fbb40981
Mr.Jeswin Baby <jeswin.b@sjri.res.in>
2017-11-25 15:14:38 +0530

    Headers

--- main_code.R
+++ main_code.R
@@ -1,6 +1,15 @@
-#First comment
+#----------------------------------------------------------------------------
+# Project             : Digit Recognition
+# Objective           : predict the digits from the images
+# Owner               : Jithin, Jeswin & Satvik
+# Created date        : 2511/17
+# Modified by         : 
+# Last Modified Date  : 
+#
+# Modules :
+#     1. Reading the data
+#     2. Cleaning the data
+#     3. Analysis
+#----------------------------------------------------------------------------
 
-#ABCDE
-##Satvik Comment
-
-#jeswin
+#-----------------Module 1: Reading data-------------------------------------
